542a2aabab42baa9b85bccddbb94d5d1eb1e6168
Lou Fontana <loufontana@gmail.com>
2016-01-11 11:40:30 -0700

    Added ability to view skills in profile view

--- public/script/app.js
+++ public/script/app.js
@@ -124,11 +124,12 @@ function viewServiceRecord(id) {
   }).done(function(members) {
     data = {
       member: members.members[0],
+      skills: members.skills,
       user: appvars.user,
       deletable: appvars.user && (appvars.user.id == members.members[0].id || appvars.user.role_name == 'admin'),
       bannable: appvars.user && appvars.user.role_name != 'normal' && appvars.user.id != members.members[0].id
     };
-    displayTemplate('main', 'member', data);
+    displayTemplate('main', 'member', 'skills', data);
   });
 }
 
