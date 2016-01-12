a99946b85057cc4f5d7a8948f4f79b2ba3452a04
Lou Fontana <loufontana@gmail.com>
2016-01-12 11:21:03 -0700

    fixed misc bugs

--- public/script/app.js
+++ public/script/app.js
@@ -375,6 +375,40 @@ function selectPlanitType(id) {
   $('.category-btn').html(planitType + '<span class="caret"></span>');
 }
 
+function createTask() {
+  $.ajax({
+    url: '/tasks/',
+    method: 'post'
+  }).done(function(types){
+    appvars.task_types = types.task_types
+  var data = {
+    task_types: appvars.task_types,
+    title: 'Task Creation',
+    headCount: headCount,
+    startDate: formatDateInput(Date.now()),
+    endDate: formatDateInput(Date.now())
+    };
+  displayTemplate('main', 'taskupdate', data);
+  })
+}
+
+function createTaskPost(event, id) {
+  if (event) event.preventDefault();
+  var formData = getFormData('form');
+  $.ajax({
+    url: '/tasks',
+    method: 'post',
+    data: formData,
+    xhrFields: {
+      withCredentials: true
+    }
+  }).done(function(data) {
+    viewTask(data.tasks[0].id);
+  }).fail(function(err){
+    customAlert('All filed must be filled out to create a task')
+  });
+}
+
 function listTasks() {
   $.ajax({
     url: '/tasks',
@@ -392,40 +426,46 @@ function viewTask(id) {
   }).done(function(tasks) {
     data = {
       task: tasks.tasks[0],
-      tasks: Tasks.tasks,
-      user: appvars.user
+      //? tasks: Tasks.tasks,
+      user: appvars.user,
+      editable: appvars.user && (appvars.user.id == tasks.tasks[0].member_id || appvars.user.role_name == 'admin'),
+      deletable: appvars.user && (appvars.user.id == tasks.tasks[0].member_id || appvars.user.role_name !== 'normal')
     };
     displayTemplate('main', 'task', data);
   });
 }
 
+// May be a mistake
+
 function updateTask(id) {
+  Promise.all([
   $.ajax({
     url: '/tasks/' + id,
     method: 'get'
-  }).done(function(tasks) {
-    displayTemplate('main', 'taskupdate', tasks.tasks[0]);
-  });
-}
-
-function createTask() {
-  displayTemplate('main', 'taskupdate');
-}
-
-function createTaskPost(event, id) {
-  if (event) event.preventDefault();
-  var formData = getFormData('form');
+  }),
   $.ajax({
-    url: '/tasks',
-    method: 'post',
-    data: formData,
-    xhrFields: {
-      withCredentials: true
-    }
-  }).done(function(data) {
-    viewTask(id);
+    url: '/tasks/task_types',
+    method: 'get'
+  })
+  ]).then(function(data) {
+    appvars.task_types = data[1].task_types;
+    var task = data[0].tasks[0];
+    var data = {
+      tasks: task,
+      task_types: data[1].task_types,
+      title: 'Task Update',
+      update: true,
+      headCount: headCount,
+      budget: budget,
+      startDate: formatDateInput(task.start_date),
+      endDate: formatDateInput(task.end_date)
+    };
+    displayTemplate('main', 'taskupdate', data);
   });
 }
+
+
+//Unsure if id is needed as well
 
 function updateTaskPut(event, id) {
   if (event) event.preventDefault();
@@ -458,4 +498,10 @@ function deleteTask(id) {
       }
     });
   });
+}
+
+function selectTaskType(id) {
+  $('.task-type').val(id);
+  var taskType = findBy(appvars.task_types, 'id', id).name;
+  $('.category-btn').html(taskType + '<span class="caret"></span>');
 }
