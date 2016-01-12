5dc757b844fa80589203eec60f2228d16f9b2b52
Adam Oken <Adam_Oken@Adams-MacBook-Air.local>
2016-01-11 20:36:26 -0700

    fixed dropdown skills and planit types

--- public/script/app.js
+++ public/script/app.js
@@ -273,7 +273,7 @@ function updatePlanit(id) {
     var data = {
       planit: data[0].planits[0],
       planit_types: data[1].planit_types,
-      skills: data[2]
+      skills: data[2].skills
     };
     console.log(data);
     displayTemplate('main', 'planitupdate', data);
