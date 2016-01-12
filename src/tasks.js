4713c38789e58514d3e7a8295e212add3f80985b
Elana Kopelevich <ekopelevich@gmail.com>
2016-01-11 17:36:39 -0700

    Created tasks scaffolding

---
+++ src/tasks.js
@@ -0,0 +1,65 @@
+function listTasks() {
+  $.ajax({
+    url: '/tasks',
+    method: 'get'
+  }).done(function(tasks) {
+    Tasks.user = appvars.user;
+    displayTemplate('main', 'tasks', tasks);
+  });
+}
+
+function viewTask(id) {
+  $.ajax({
+    url: '/tasks/' + id,
+    method: 'get'
+  }).done(function(tasks) {
+    data = {
+      task: tasks.tasks[0],
+      tasks: Tasks.tasks,
+      user: appvars.user
+    };
+    displayTemplate('main', 'task', data);
+  });
+}
+
+function updateTask(id) {
+  $.ajax({
+    url: '/tasks/' + id,
+    method: 'get'
+  }).done(function(tasks) {
+    displayTemplate('main', 'taskupdate', Tasks.Tasks[0]);
+  });
+}
+
+function updateTaskPut(event, id) {
+  if (event) event.preventDefault();
+  var formData = getFormData('form');
+  $.ajax({
+    url: '/tasks/' + id,
+    method: 'put',
+    data: formData,
+    xhrFields: {
+      withCredentials: true
+    }
+  }).done(function(data) {
+    viewTask(id);
+  });
+}
+
+function deleteTask(id) {
+  customConfirm('Are you sure you want to delete this task?', function() {
+    $.ajax({
+      url: '/tasks/' + id,
+      method: 'delete',
+      xhrFields: {
+        withCredentials: true
+      }
+    }).done(function(data) {
+      if (id == appvars.user.id) {
+        logout();
+      } else {
+        displayTemplate('main', 'splashpage');
+      }
+    });
+  });
+}
