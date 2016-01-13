ff9f30bd61b047a251a7840e9c7d625e887877f8
Ben Allred <skuttleman@gmail.com>
2016-01-13 06:18:17 -0700

    preliminary proposal-routes

--- routes/proposals.js
+++ routes/proposals.js
@@ -1,126 +1,93 @@
-module.exports=require('express').Router();
-//
-// var route = require('express').Router();
-// var proposals = require('./proposals');
-// var knex = require('../db/knex');
-// module.exports = route;
-//
-//
-// route.use('/:id', function(request, response, next) {
-//   request.routeChain = request.routeChain || {};
-//   request.routeChain.proposalId = request.params.id;
-//   next();
-// });
-//
-// route.use('/:id/proposals', proposals);
-//
-//
-//
-// // C
-// route.post('/', function(request, response, next) {
-//   if (request.user && request.routeChain && request.routeChain.proposalId) {
-//     var body = digest(request.body, request.routeChain.proposalId);
-//     knex('proposals').returning('*').insert(body.body).then(function(proposals) {
-//       if (body.description) {
-//         return knex('proposal_details').insert({ id: proposals[0].id, description: description }).then(function() {
-//           return Promise.resolve(proposals);
-//         });
-//       } else {
-//         return Promise.resolve(proposals);
-//       }
-//     }).then(function(proposals) {
-//       response.json({ success: true, proposals: proposals });
-//     });
-//   }
-// });
-//
-// // R
-// - LF - pretty sure this route is horked
-// route.get('/:id', function(request, response, next) {
-//   var where = {
-//     'proposals.id': request.params.id
-//   };
-//   if (request.routeChain && request.routeChain.proposalId) where.proposal_id = request.routeChain.proposalId;
-//   else response.json({ sucess: false });
-//   Promise.all([
-//     knex('proposals').select('proposals.id as proposal_id', 'proposals.*',)
-//     knex('proposals').where({ id: where.proposal_id })
-//   ]).then(function(data) {
-//     var proposals = data[0];
-//     var memberId = data[1][0].member_id;
-//     console.log('memberid', memberId);
-//     proposals.forEach(function(proposalId) {
-//       proposal.id = proposalId.proposalId_id;
-//       proposal.task_id = memberId;
-//       delete proposal.proposal_id;
-//     });
-//     response.json({ success: true, proposals: proposals });
-//   }).catch(function(err) {
-//     console.log(err);
-//   });
-// });
-//
-// // U
-// route.put('/:id', function(request, response, next) {
-//   if (request.user.id) {
-//     var body = digest(request.body);
-//     Promise.all([
-//       knex('proposals').where('id', request.params.id).update(body.body),
-//       knex('proposal_details').where('id', request.params.id)
-//     ]).then(function(data) {
-//       var proposalId = data[0][0];
-//       var skillDescription = data[1][0];
-//       return updateOrCreate(description, skillDescription, request.params.id)
-//       .then(function() {
-//         return Promise.resolve(data[0]);
-//       });
-//     }).then(function(proposals) {
-//       response.json({ success: true, proposals: proposals });
-//     });
-//   }
-// });
-//
-// // D
-// route.delete('/:id', function(request, response, next) {
-//   if (request.user.id) {
-//     knex('proposals').where({ id: request.params.id }).del().then(function() {
-//       response.json({ success: true });
-//     });
-//   }
-// });
-//
-// // L
-// route.get('/', function(request, response, next) {
-//   var where = {};
-//   if (request.routeChain && request.routeChain.proposalId) where.proposal_id = request.routeChain.proposalId;
-//   knex('proposals').select('proposals.id as proposalId_id', 'proposals.*', 'proposal_details.*', 'skills.*')
-//   .where(where).then(function(proposals) {
-//     proposals.forEach(function(proposalId) {
-//       proposal.id = proposal.proposal_id;
-//       delete proposal.proposal_id;
-//     });
-//     response.json({ success: true, proposals: proposals });
-//   });
-// });
-//
-// function digest(body, id) {
-//   var ret = {
-//     description: body.description
-//   };
-//   if (body.hasOwnProperty('id')) delete body.id;
-//   if (body.hasOwnProperty('description')) delete body.description;
-//   ret.body = body;
-//   return ret;
-// }
-//
-// function updateOrCreate(text, record, id) {
-//   if (record && text) {
-//     return knex('proposal_details').where({ id: record.id }).update({ description: text });
-//   } else if (record) {
-//     return knex('proposal_details').where({ id: record.id }).del();
-//   } else if (text) {
-//     return knex('proposal_details').where({ id: record.id }).insert({ id: id, description: text });
-//   } else {
-//     return Promise.resolve();
-//   }
-// }
+var route = require('express').Router();
+var knex = require('../db/knex');
+
+module.exports = route;
+
+
+
+route.use('/:id', function(request, response, next) {
+  request.routeChain = request.routeChain || {};
+  request.routeChain.proposals = request.params.id;
+  next();
+});
+
+
+
+// C
+route.post('/', function(request, response, next) {
+  if (request.user && request.routeChain && request.routeChain.taskId) {
+    request.body.task_id = request.routeChain.taskId;
+    request.body.member_id = request.user.id;
+    knex('proposals').insert(request.body).then(function() {
+      response.json({ success: true });
+    });
+  } else {
+    next('You must be logged in to preform this action');
+  }
+});
+
+// R
+route.get('/:id', function(request, response, next) {
+  knex('proposals').where({ id: request.params.id }).then(function(proposals) {
+    response.json({ success: true, proposals: proposals });
+  });
+});
+
+// U
+route.put('/:id', function(request, response, next) {
+  if (request.user) {
+    connectProposalToPlanit.then(function(data) {
+      var planit = data.planits[0], proposal = data.proposals[0], task = data.tasks[0];
+      if (request.user.id == proposal.member_id || request.user.id == planit.member_id
+      || request.user.role_name == 'admin') {
+        knex('proposals').where({ id: request.params.id }).update(request.body);
+      } else {
+        next('You do not have permission to perform this action');
+      }
+    });
+  } else {
+    next('You must be logged in to perform this action');
+  }
+});
+
+// D
+route.delete('/:id', function(request, response, next) {
+  if (request.user) {
+    knex('proposals').where({ id: request.params.id }).then(function(proposals) {
+      var proposal = proposals[0];
+      if (request.user.id == proposal.member_id || request.user.role_name == 'admin') {
+        knex('proposals').where({ id: request.params.id }).del().then(function() {
+          response.json({ success: true });
+        }).catch(function(err) {
+          next(err);
+        });
+      } else {
+        next('You do not have permission to perform this action');
+      }
+    });
+  } else {
+    next('You must be logged in to perform this action');
+  }
+});
+
+// L
+route.get('/', function(request, response, next) {
+  if (request.routeChain && request.routeChain.taskId) {
+    knex('proposals').where({ task_id: request.routeChain.taskId }).then(function(proposals) {
+      response.json({ success: true, proposals: proposals });
+    });
+  }
+});
+
+function connectProposalToPlanit(id) {
+  return knex('proposals').where({ id: id }).then(function(proposals) {
+    return knex('tasks').where({ id: proposals[0].task_id }).then(function(tasks) {
+      return Promise.resolve({ tasks: tasks, proposals: proposals });
+    });
+  }).then(function(data) {
+    return knex('planits').where({ id: data.tasks[0].planit_id }).then(function(planits) {
+      data.planits = planits;
+      return Promise.resolve(data);
+    });
+  });
+}
