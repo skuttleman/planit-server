c20c6e0a55bcef2213d98c647fa642041ae3a914
Adam Oken <Adam_Oken@Adams-MacBook-Air.local>
2016-01-13 12:55:36 -0700

    working on dropdown with key navigation

--- src/style.js
+++ src/style.js
@@ -4,13 +4,29 @@ $(document).ready(function() {
 
 
 
-// $(".createplanit").click(function() {
-// 	var title = $('.titletest');
-// 	if(!title.val()) {
-// 		alert('nice')
-//     }
-//     else{
-//     	alert('enter a title fool')
-//     }
-// })
+$('.planit-dropdown').on('dropdown-menu', function () {
+	console.log('hello')
+  
+    var $this = $(this);
+    // attach key listener when dropdown is shown
+    $(document).keypress(function(e){
+      
+      // get the key that was pressed
+      var key = String.fromCharCode(e.which);
+      // look at all of the items to find a first char match
+      $this.find("li").each(function(idx,item){
+        $(item).removeClass("keybind-dropdown"); // clear previous active item
+        if ($(item).text().charAt(0).toLowerCase() == key) {
+          // set the item to selected (active)
+          $(item).addClass("keybind-dropdown");
+        }
+      });
+      
+	});
+  
+})
 
+// unbind key event when dropdown is hidden
+$('.planit-dropdown').on('dropdown-menu', function () {
+    $(document).unbind("keypress");
+})
