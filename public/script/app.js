63cdcc9badc76b2d638b67bfc7af998031985dc7
Elana Kopelevich <ekopelevich@gmail.com>
2016-01-14 20:32:07 -0700

    mission control progress

--- public/script/app.js
+++ public/script/app.js
@@ -397,13 +397,18 @@ function missionControl(id) {
   $.ajax({
     url: '/members/' + id,
     method: 'get'
-  }).done(function(members) {
-    data = {
+  })
+  // ,$.ajax({
+  //   url: '/members/' + id + '/planits/',
+  //   method: 'get'
+  // }).
+  .done(function(members, planits) {
+    var data = {
       member: members.members[0],
-      // skills: members.skills,
       // user: appvars.user,
       // deletable: appvars.user && (appvars.user.id == members.members[0].id || appvars.user.role_name == 'admin'),
-      // bannable: appvars.user && appvars.user.role_name != 'normal' && appvars.user.id != members.members[0].id
+      // bannable: appvars.user && appvars.user.role_name != 'normal' && appvars.user.id != members.members[0].id,
+      // planits: planits.planit[0]
     };
     displayTemplate('main', 'dashboard', data);
   });
